395a2b241d7d65e421a825bdc6485703327f5dc3
Adam Hoffman <S00197432@mail.itsligo.ie>
2021-08-12 15:30:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatLab2OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
